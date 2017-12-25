3050ed60451731fa68ee4836f0b4301bd52595b7
qzsecfr <qzsecfr@outlook.com>
2017-12-25 10:52:20 +0800

    good

---
+++ tutorial01/build/CMakeFiles/leptjson_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leptjson_test.dir/test.c.obj"
+  "leptjson_test.pdb"
+  "leptjson_test.exe"
+  "leptjson_test.exe.manifest"
+  "leptjson_test.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leptjson_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
